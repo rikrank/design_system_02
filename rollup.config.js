002b795cd58171343f079bd2a9c46430ba3d0cf5
rikrank <morishita-r@tam-tam.co.jp>
2022-11-15 18:33:07 +0900

    add：postcss

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 const resolve = require('@rollup/plugin-node-resolve')
 const commonjs = require('@rollup/plugin-commonjs')
 const typescript = require('@rollup/plugin-typescript')
+const postcss = require("rollup-plugin-postcss");
 const dts = require('rollup-plugin-dts')
 
 const packageJson = require("./package.json");
@@ -24,11 +25,13 @@ module.exports = [
             resolve(),
             commonjs(),
             typescript({ tsconfig: './tsconfig.json' }),
+            postcss(),
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts.default()],
+        external: [/\.css$/],
     },
 ]
